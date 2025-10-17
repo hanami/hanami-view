6db6c458c2e40765f0aaf9b575a1bd25090d2cc6
Tim Riley <tim@riley.id.au>
2025-10-17 20:28:43 +1100

    Remove coverage from CI

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,5 @@
 # frozen_string_literal: true
 
-require_relative "support/coverage"
-
 begin
   require "pry-byebug"
 rescue LoadError; end
--- spec/support/coverage.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-# this file is managed by dry-rb/devtools
-
-if ENV["COVERAGE"] == "true"
-  require "codacy-coverage"
-
-  Codacy::Reporter.start
-end
